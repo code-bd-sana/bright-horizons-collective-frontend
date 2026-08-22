3e50d3757310a81ac023fa114f97c8e8bc36b406
KhalidTheCoder <khalidahsan2407@gmail.com>
2026-08-22 12:01:17 +0600

    security: remove compromised PostCSS payload

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,6 @@
 import { AboutFaqSection } from '@/components/about/about-faq-section';
 import { AboutFinalCtaSection } from '@/components/about/about-final-cta-section';
 import { AboutHero } from '@/components/about/about-hero';
-import { AboutImpactSection } from '@/components/about/about-impact-section';
 import { AboutJourneySection } from '@/components/about/about-journey-section';
 import { AboutStorySection } from '@/components/about/about-story-section';
 import { AboutSupportSection } from '@/components/about/about-support-section';
@@ -11,7 +10,6 @@ export default function AboutPage() {
   return (
     <main>
       <AboutHero />
-      {/* <AboutImpactSection /> */}
       <AboutStorySection />
       <AboutSupportSection />
       <AboutValuesSection />
