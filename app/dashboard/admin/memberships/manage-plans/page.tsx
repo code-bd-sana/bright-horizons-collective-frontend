3e28aeea26f01f16c2ad74a4b8541c4ec1e1f368
FyslAhmd <info.faysal.32@gmail.com>
2026-09-01 10:11:37 +0600

    feat: add ManageMembershipPlansPage and ManageMembershipPlansRoute for managing membership plans

---
+++ app/dashboard/admin/memberships/manage-plans/page.tsx
@@ -0,0 +1,5 @@
+import { ManageMembershipPlansPage } from '@/components/dashboard/admin/memberships/manage-membership-plans-page';
+
+export default function ManageMembershipPlansRoute() {
+  return <ManageMembershipPlansPage />;
+}
